0a408069bca2e8c74bcbdab99bcd5eb0163d6f6b
Ho Jie Feng <hjf3108@gmail.com>
2020-10-31 15:19:36 +0800

    adr size test

--- adr2.py
+++ adr2.py
@@ -9,16 +9,16 @@ def adr_strategy(vale_trade_info, valbz_trade_info) -> dict:
         result = adr_signal(valbz, vale)
         if(result != None and result[0] == True):
             print ("\n------------------------- ADR Trading -------------------------\n")
-            #size_to_trade = (result[3] - 2) * 2 + 10
-            stuff = result[3] // 2
-            return [{"type" : "add", "symbol": "VALE", "dir" : "BUY", "price": result[1]+stuff, "size": 10},
+            size_to_trade = (result[3] - 2) * 2 + 4
+            #stuff = result[3] // 2
+            return [{"type" : "add", "symbol": "VALE", "dir" : "BUY", "price": result[1]+1, "size": size_to_trade},
                     {"type" : "convert", "symbol": "VALE", "dir" : "SELL", "size": 10},
-                    {"type" : "add", "symbol": "VALBZ", "dir" : "SELL", "price": result[2]-stuff, "size": 10}]
+                    {"type" : "add", "symbol": "VALBZ", "dir" : "SELL", "price": result[2]-1, "size": size_to_trade}]
 
 #common stock & its ADR pair trading strategy
 def adr_signal(cs_trade_price_list, adr_trade_price_list):
     cs_mean = mean(cs_trade_price_list)
     adr_mean = mean(adr_trade_price_list)
     fair_diff = cs_mean - adr_mean
-    if (fair_diff >= 1):
+    if (fair_diff >= 2):
         return [True, adr_mean, cs_mean, fair_diff]
