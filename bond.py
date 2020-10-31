307d4221e9d68d4f70824c46451ca551a922c8f1
Ho Jie Feng <hjf3108@gmail.com>
2020-10-31 16:40:39 +0800

    add protection for bond

--- bond.py
+++ bond.py
@@ -8,12 +8,14 @@ def bond_strategy(buy, sell) -> dict:
     if buy and buy[0][0] > 1000:
         buy_trades = []
         for i in range(len(buy)):
-            buy_trades.append({"type": "add", "symbol": "BOND", "dir": "SELL", "price": buy[i][0], "size": buy[i][1]})
+            if buy[i][0] > 1000:
+                buy_trades.append({"type": "add", "symbol": "BOND", "dir": "SELL", "price": buy[i][0], "size": buy[i][1]})
         return buy_trades
     elif sell and sell[0][0] < 1000:
         sell_trades = []
         for i in range(len(sell)):
-            sell_trades.append({"type": "add", "symbol": "BOND", "dir": "BUY", "price": sell[i][0], "size": sell[i][1]})
+            if sell[i][0] < 1000:
+                sell_trades.append({"type": "add", "symbol": "BOND", "dir": "BUY", "price": sell[i][0], "size": sell[i][1]})
         return sell_trades
     else:
         return None
