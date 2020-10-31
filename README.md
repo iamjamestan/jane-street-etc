0a0bb63e9297a5d3d60ce8c9cb18820e7f535868
Ho Jie Feng <hjf3108@gmail.com>
2020-10-31 20:09:30 +0800

    Add bond readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # jane-street-etc
+
+
+## BOND Strategy
+
+We used a very simple strategy with a speed optimization. Sell when price is above 1000 and buy when price is below 1000. Additional speed comes in buying up all the orders which meet the criterion instead of only 1. 
+
--- bond.py
+++ bond.py
@@ -5,17 +5,18 @@ returns {"type": "add", "order_id": N, "symbol": "SYM", "dir": "BUY", "price": N
 
 
 def bond_strategy(buy, sell) -> dict:
-    if buy and buy[0][0] > 1000:
-        buy_trades = []
-        for i in range(len(buy)):
-            if buy[i][0] > 1000:
-                buy_trades.append({"type": "add", "symbol": "BOND", "dir": "SELL", "price": buy[i][0], "size": buy[i][1]})
-        return buy_trades
-    elif sell and sell[0][0] < 1000:
-        sell_trades = []
-        for i in range(len(sell)):
-            if sell[i][0] < 1000:
-                sell_trades.append({"type": "add", "symbol": "BOND", "dir": "BUY", "price": sell[i][0], "size": sell[i][1]})
-        return sell_trades
+#if buy and buy[0][0] > 1000:
+    buy_trades = []
+    for i in range(len(buy)):
+        if buy[i][0] > 1000:
+            buy_trades.append({"type": "add", "symbol": "BOND", "dir": "SELL", "price": buy[i][0], "size": buy[i][1]})
+    #    return buy_trades
+    #elif sell and sell[0][0] < 1000:
+    sell_trades = []
+    for i in range(len(sell)):
+        if sell[i][0] < 1000:
+            sell_trades.append({"type": "add", "symbol": "BOND", "dir": "BUY", "price": sell[i][0], "size": sell[i][1]})
+    #    return sell_trades
+    return buy_trades + sell_trades
     else:
         return None
