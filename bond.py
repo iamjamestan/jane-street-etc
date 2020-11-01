46fc7ad223551b03055bb72d086dd286a8b65ed3
Hanming Zhu <hanming@huawen.com>
2020-11-01 18:06:27 +0800

    Touch up bond strategy

--- bond.py
+++ bond.py
@@ -1,26 +1,29 @@
 from typing import List
-from etc_types import Trade
-"""
-Takes in `buy` and `sell` arrays and returns an array of trades to perform.
-These trades will need `order_id` to be added separately.
+from etc_types import *
 
-Example Input:
-`buy` = [[1001, 4], [999, 8]]
-`sell` = [[998, 5], [1002, 7]]
+def bond_strategy(buy: RestingList, sell: RestingList) -> List[Trade]:
+    """Takes in `buy` and `sell` resting orders and returns trades to perform.
+        These trades will need `order_id` to be added separately.
 
-Example Output:
-[
-    { "type": "add", "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 4 },
-    { "type": "add", "symbol": "BOND", "dir": "BUY", "price": 998, "size": 5 }
-]
-"""
-def bond_strategy(buy: List[List[int]], sell: List[List[int]]) -> List[Trade]:
+    Args:
+        buy (List[List[int]]): Resting buy orders of BOND.
+        sell (List[List[int]]): Resting sell orders of BOND.
+
+    Returns:
+        List[Trade]: List of trades to perform. May be empty.
+    
+    Examples:
+        >>> bond_strategy([[1001, 4], [999, 8]], [[998, 5], [1002, 7]])
+        [{"type": "add", "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 4},
+        {"type": "add", "symbol": "BOND", "dir": "BUY", "price": 998, "size": 5}]
+
+    """
     buy_trades = []
     for i in range(len(buy)):
         if buy[i][0] > 1000:
-            buy_trades.append({"type": "add", "symbol": "BOND", "dir": "SELL", "price": buy[i][0], "size": buy[i][1]})
+            buy_trades.append({"type": Action.ADD, "symbol": Symbol.BOND, "dir": Direction.SELL, "price": buy[i][0], "size": buy[i][1]})
     sell_trades = []
     for i in range(len(sell)):
         if sell[i][0] < 1000:
-            sell_trades.append({"type": "add", "symbol": "BOND", "dir": "BUY", "price": sell[i][0], "size": sell[i][1]})
+            sell_trades.append({"type": Action.ADD, "symbol": Symbol.BOND, "dir": Direction.BUY, "price": sell[i][0], "size": sell[i][1]})
     return buy_trades + sell_trades
